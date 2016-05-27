a8c7cc98cdd73216e7add0f4f950ebab6d4a9b60
wenroule <512344238@qq.com>
2016-05-28 00:24:11 +0800

    修改01

--- src/com/atguigu/git/test/Hello.java
+++ src/com/atguigu/git/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("1111");
 		System.out.println("github更新01");
+		System.out.println("local更新02");
 	}
 	
 }
--- src/com/atguigu/git/test/Test01.java
+++ src/com/atguigu/git/test/Test01.java
@@ -3,8 +3,7 @@ package com.atguigu.git.test;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
